bb233f75029444426ac4a8e728ec9688b51f3cd3
ryunze <ryunzeroeight08@gmail.com>
2026-04-30 00:03:12 +0800

    fix: no

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
     import Hero from './components/Hero.vue';
     import Navbar from './components/Navbar.vue'
+import SectionFirst from './components/SectionFirst.vue';
 </script>
 
 <template>
@@ -8,16 +9,6 @@
     <Navbar />
     <Hero />
 
-    <section class="px-30 grid grid-cols-2 gap-12 mt-12">
-        <div class="w-full h-100 bg-red-100"></div>
-        <div class="w-full h-100">
-            <h2 class="text-6xl font-bold mb-8">Technical Views</h2>
-            <p class="text-xl">A research tool that combines automated<br/>algorithm with expert market analysis to<br/>help you create better trading plans.</p>
-            <div class="flex mt-8">
-                <a href="#" class="w-max block bg-blue-500 text-white py-3 px-12 rounded-full me-6">Learn More</a>
-                <a href="#" class="w-max block outline-3 outline-blue-500 text-blue-500 py-3 px-12 rounded-full">Explore</a>
-            </div>
-        </div>
-    </section>
+    <SectionFirst/>
 
 </template>
---
+++ src/components/SectionFirst.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="px-30 grid grid-cols-2 gap-12 mt-12">
+        <div class="w-full h-100 bg-red-100">
+            <div class="rounded-b-lg flex bg-indigo-100 ">
+                <div class="flex bg-blue-100 p-2 w-35 rounded-full">
+                    <h4>Technical Views</h4>
+                </div>
+                <div class="flex bg-blue-100 p-2 w-35 rounded-full">
+                    <h4>Technical Views</h4>
+                </div>
+            </div>
+        </div>
+        <div class="w-full h-100">
+            <h2 class="text-6xl font-bold mb-8">Technical Views</h2>
+            <p class="text-xl">A research tool that combines automated<br/>algorithm with expert market analysis to<br/>help you create better trading plans.</p>
+            <div class="flex mt-8">
+                <a href="#" class="w-max block bg-blue-500 text-white py-3 px-12 rounded-full me-6">Learn More</a>
+                <a href="#" class="w-max block outline-3 outline-blue-500 text-blue-500 py-3 px-12 rounded-full">Explore</a>
+            </div>
+        </div>
+    </section>
+</template>
